11b34c0cfa1190c80d116bca2e7b2b340e45c8fb
Keith A. Lewis <kal@kalx.net>
2024-05-01 11:03:49 -0400

    sync

---
+++ md.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Make .md into html
+
+#FLAGS += --metadata date="$(shell date "+%B %e, %Y")"
+
+echo $@
+
+for md in "$@"; do
+	echo $md
+	date=$(stat -c "%y" ${md} | cut -d' ' -f 1)
+	dn=$(dirname ${md})
+	echo $date
+	#echo "pandoc ${md} --metadata date=\"${date}\" -o ${dn}.html"
+done
